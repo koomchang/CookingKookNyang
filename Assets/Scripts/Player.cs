9854ee50b396aa57728d965cd1fadc52accae91d
cjkim <cjkim@cjkimui-MacBookPro.local>
2023-10-04 15:09:25 +0900

    feat: add moveSpeed var

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-
-
 {
+    [SerializeField] private float moveSpeed = 7f;
     private void Update() {
         
         Vector2 inputVector = new Vector2(0,0); // 키보드 상 x, y 축만 존재
@@ -26,7 +25,7 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); // x, y, z 축
-        transform.position += moveDir;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
         Debug.Log(inputVector);
     }
 }
