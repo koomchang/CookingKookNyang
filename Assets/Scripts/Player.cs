23976044b2008e7f9fad609ab9f905bcbeccc0aa
cjkim <cjkim@cjkimui-MacBookPro.local>
2023-10-04 14:57:23 +0900

    feat: player object movement implement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+
+{
+    private void Update() {
+        
+        Vector2 inputVector = new Vector2(0,0); // 키보드 상 x, y 축만 존재
+
+        if (Input.GetKey(KeyCode.W)) { // 상
+            inputVector.y = +1;
+        } 
+        if (Input.GetKey(KeyCode.S)) { // 하
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A)) { // 좌
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) { // 우
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); // x, y, z 축
+        transform.position += moveDir;
+        Debug.Log(inputVector);
+    }
+}
