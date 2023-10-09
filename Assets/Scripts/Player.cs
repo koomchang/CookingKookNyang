3e5049d7d4bf642bc3aab40afcf16b198cf9492e
cjkim <cjkim@cjkimui-MacBookPro.local>
2023-10-09 21:57:57 +0900

    feat: Implement CatIdle, CatWalk Anim & Delete CrazyCat Assets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
     private void Update() {
         
         Vector2 inputVector = new Vector2(0,0); // 키보드 상 x, y 축만 존재
@@ -27,7 +29,13 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); // x, y, z 축
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 10f; // 방향전환 스피드
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
