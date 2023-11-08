25882c6626b25aee241c55b3eb9db2062a5c8876
koomchang <leahpar0401@gmail.com>
2023-11-08 23:31:39 +0900

    feat: 빈 테이블에 물건을 놓고 다시 들어올릴 수 있도록 구현

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,6 +7,18 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-	
-    }
+		if (!HasKitchenObject()) {
+			if (player.HasKitchenObject()) {
+			    player.GetKitchenObject().SetKitchenObjectParent(this);
+    		} else {
+				
+			}
+		} else {
+			if (player.HasKitchenObject()) {
+                
+            } else {
+				GetKitchenObject().SetKitchenObjectParent(player);
+			}
+		}
+	}
 }
