65b122700a2708f3855773ae4b472a851fd8d010
koomchang <leahpar0401@gmail.com>
2023-11-17 00:51:08 +0900

    feat: 식재료 아이콘이 음식 위에 뜨도록 구현

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlateIconsSingleUI : MonoBehaviour {
+	[SerializeField] private Image image;
+	public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO) {
+		image.sprite = kitchenObjectSO.sprite;
+	}
+}
