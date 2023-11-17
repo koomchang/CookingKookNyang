42eaec1213378dfba19b695111b4134ae54de3db
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-17 13:44:14 +0900

    feat: implement recipe UI

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour {
+
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
+    public static DeliveryManager Instance { get; private set; } // singleton
+    [SerializeField] private RecipeListSO recipeListSO;
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+
+    private void Awake() {
+        Instance = this;
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update() {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f) {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax) {
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject) {
+        for (int i=0; i<waitingRecipeSOList.Count; i++) {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count) {
+                // 만약 레시피의 식재료 갯수와 접시에 담긴 식재료의 갯수가 같다면
+                bool plateContentMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList) {
+                    // 레시피에 담긴 모든 식재료들을 순회하는 동시에
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()) {
+                        // 접시에 담긴 모든 식재료들을 순회하고
+                        if (plateKitchenObjectSO == recipeKitchenObjectSO) {
+                            // 만약 레시피 오브젝트와 접시 오브젝트가 동일하다면 재료는 잘 가져온 것으로 판단
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFound) {
+                        // 만약 레시피 오브젝트와 접시 오브젝트가 동일하지 않다면 재료조차 잘 가져오지 못한 것으로 판단
+                        plateContentMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentMatchesRecipe) {
+                    // 만약 plateContentMatchesRecipe 가 참이라면 레시피대로 재료를 잘 가져온 것으로 판단
+                    waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    return;
+                }
+            }
+        }
+
+        // 만약 plateContentMatchesRecipe 가 거짓이라면 레시피대로 재료를 가져오지 않은 것으로 판단
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
+    }
+
+}
