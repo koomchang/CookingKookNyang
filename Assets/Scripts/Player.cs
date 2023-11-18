ffa4cec87a1e80fefecee0728e8359bc3a4eb4ea
KimChanJin97 <cjkimhello97@gmail.com>
2023-11-18 09:37:49 +0900

    feat: implement GameStartCountdownUI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -60,6 +60,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 	}
 
 	private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
+		if (!KitchenGameManager.Instance.IsGamePlaying()) return;
 		if (selectedCounter != null) selectedCounter.InteractAlternate(this);
 	}
 
