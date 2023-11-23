0bb2b6d4fea788e0c2678e09c815cae86910a2ea
koomchang <leahpar0401@gmail.com>
2023-11-24 00:36:08 +0900

    fix: 게임 시간 60초로 설정

--- Assets/Scripts/KitchenGameManager.cs
+++ Assets/Scripts/KitchenGameManager.cs
@@ -24,7 +24,7 @@ public class KitchenGameManager : MonoBehaviour
     
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 120f;
+    private float gamePlayingTimerMax = 60f;
     private bool isGamePaused = false;
 
 
