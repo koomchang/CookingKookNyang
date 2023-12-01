665663fb2c2e93c5080edfbc1f60ae84763f2877
KimChanJin97 <cjkimhello97@gmail.com>
2023-12-01 15:23:50 +0900

    fix: modify LobbyScene and UI

--- Assets/Scripts/UI/LobbyCreateUI.cs
+++ Assets/Scripts/UI/LobbyCreateUI.cs
@@ -10,7 +10,7 @@ public class LobbyCreateUI : MonoBehaviour {
     [SerializeField] private Button closeButton;
     [SerializeField] private Button createPublicButton;
     [SerializeField] private Button createPrivateButton;
-    [SerializeField] private TMP_InputField lobbyNameInputField;
+    [SerializeField] private InputField lobbyNameInputField;
 
 
 
